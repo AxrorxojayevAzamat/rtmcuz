6ca47c65ef858586c304f4ff2a311f592ede5eb9
Azamat <axrorxojayevazamat@gmail.com>
2022-11-30 10:56:44 +0500

    fixes and changes

--- rtmcuz/Controllers/HomeController.cs
+++ rtmcuz/Controllers/HomeController.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Localization;
 using Microsoft.AspNetCore.Localization;
 using rtmcuz.Extensions;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
 
 namespace rtmcuz.Controllers
 {
@@ -19,14 +20,16 @@ namespace rtmcuz.Controllers
         private readonly RtmcUzContext _context;
         private readonly Locales _locale;
         private readonly IStringLocalizer<SlugResource> _localizer;
+        private readonly ICompositeViewEngine _compositeViewEngine;
         const string DASHBOARD = "dashboard";
         const string SHOW = "Show";
         const string NEWS = "news";
 
-        public HomeController(RtmcUzContext context, IStringLocalizer<SlugResource> localizer)
+        public HomeController(RtmcUzContext context, IStringLocalizer<SlugResource> localizer, ICompositeViewEngine compositeViewEngine)
         {
             _context = context;
             _localizer = localizer;
+            _compositeViewEngine = compositeViewEngine;
             _locale = (Locales)Enum.Parse(typeof(Locales), CultureInfo.CurrentCulture.Name.Replace('-', '_'));
         }
 
@@ -53,13 +56,19 @@ namespace rtmcuz.Controllers
         }
 
         [Route("/{slug}")]
-        public async Task<IActionResult> Section(string slug, int? page)
+        public IActionResult? Section(string slug, int? page)
         {
             if (slug.Contains(".css") || slug.Contains(".js")) return null;
             //var searchKey = GetLocaleKey(slug);
 
             SectionTypes? sectionType = GetSectionType(slug ?? "/", out string sectionName);
-            if (sectionType == SectionTypes.Static) return View(sectionName);
+            if (sectionType == SectionTypes.Static)
+            {
+                var result = ExistsView(sectionName);
+                if (!result.Success) return Redirect("/");
+
+                return View(sectionName);
+            }
 
             var query = QueryForSections((SectionTypes)sectionType).OrderByDescending(n => n.UpdatedDate);
             var items = query.ToList();
@@ -79,7 +88,7 @@ namespace rtmcuz.Controllers
         }
 
         [Route("/{slugGroup}/{slug}")]
-        public async Task<IActionResult> Show(string slugGroup, string slug)
+        public IActionResult Show(string slugGroup, string slug)
         {
             var item = _context.Sections.Include(s => s.Image).Where(s => s.Slug.Equals(slug)).FirstOrDefault();
             if (item == null)
@@ -95,30 +104,36 @@ namespace rtmcuz.Controllers
 
             if (!item.Lang.Equals(_locale))
             {
-                await SetCookie(item.Lang.ToString().Replace("_", "-"));
+                SetCookie(item.Lang.ToString().Replace("_", "-"));
                 return Redirect($"/{slugGroup}/{item.Slug}");
             }
 
-            string viewName = $"{item.Type.ToString()}{SHOW}";
+            string viewName = $"{item.Type}{SHOW}";
+
+            var result = ExistsView(viewName);
+            if (!result.Success) return Redirect("/");
 
             return View(viewName, item);
         }
 
         [Route("SetLanguage")]
-        public async Task<IActionResult> SetLanguage(string culture, string currentUrl)
+        public IActionResult SetLanguage(string culture, string currentUrl)
         {
 
             string[] slugs = currentUrl.Split('?')[0].Split('/').Where((val, i) => i != 0).ToArray();
+            SetCookie(culture);
+
             if (slugs[0] == DASHBOARD) return LocalRedirect(currentUrl);
+
             //slugs[0] = GetLocaleKey(slugs[0]);
-            await SetCookie(culture);
+
             string? queryParams = currentUrl.Split('?').Length > 1 ? $"?{currentUrl.Split('?')[1]}" : "";
             string returnUrl = $"~/{GetReturnUrl(slugs, culture)}{queryParams}";
 
             return LocalRedirect(returnUrl);
         }
 
-
+        #region PRIVATE-METHODS
         private string GetReturnUrl(string[] urlArray, string culture)
         {
             if (urlArray.Length == 1)
@@ -131,8 +146,10 @@ namespace rtmcuz.Controllers
                 Section? variant = _context.Sections
                     .Where(s => s.GroupId == groupId && s.Lang == (Locales)Enum.Parse(typeof(Locales), culture.Replace('-', '_')))
                     .FirstOrDefault();
+
                 if (variant == null) return urlArray[0];
-                urlArray[urlArray.Length - 1] = (string)variant.Slug;
+
+                urlArray[urlArray.Length - 1] = variant.Slug;
                 return string.Join('/', urlArray);
             }
             return "/";
@@ -142,24 +159,34 @@ namespace rtmcuz.Controllers
         {
             sectionName = string.Join(string.Empty, slug.Split("-").Select(s => s.FirstCharToUpper()).ToArray());
             string checkingSection = slug == NEWS ? sectionName : sectionName.Substring(0, sectionName.Length - 1);
-            //SectionTypes? type = (SectionTypes)Enum.Parse(typeof(SectionTypes),);
-            if (Enum.TryParse<SectionTypes>(checkingSection, out SectionTypes type))
+
+            if (Enum.TryParse(checkingSection, out SectionTypes type))
             {
                 return type;
             }
+
             return SectionTypes.Static;
         }
 
-        public async Task SetCookie(string culture)
+        public void SetCookie(string culture)
         {
             Response.Cookies.Append(
              CookieRequestCultureProvider.DefaultCookieName,
              CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
              new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) });
         }
-        private string GetLocaleKey(string slug) => _localizer.GetAllStrings().FirstOrDefault(x => x.Value == slug)?.Name;
+
+        private ViewEngineResult ExistsView(string name)
+        {
+            var viewName = $"~/Views/Home/{name}.cshtml";
+            var result = _compositeViewEngine.GetView("", viewName, false);
+            return result;
+        }
+
+        private string? GetLocaleKey(string slug) => _localizer.GetAllStrings().FirstOrDefault(x => x.Value == slug)?.Name;
         private IQueryable<Section> QueryForSections(SectionTypes type) => _context.Sections.Include(b => b.Image).Where(s => s.Type == type && s.Lang == _locale);
         private IQueryable<Section> QueryForSection(int groupId) => _context.Sections.Include(b => b.Image).Where(s => s.GroupId == groupId && s.Lang == _locale);
+        #endregion
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()

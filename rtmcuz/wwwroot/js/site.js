aa0ac8dd3c7ee94f4ec44d5654a5e18e5e197eb9
Azamat <axrorxojayevazamat@gmail.com>
2022-12-22 19:08:27 +0500

    style changes

--- rtmcuz/wwwroot/js/site.js
+++ rtmcuz/wwwroot/js/site.js
@@ -20,3 +20,14 @@ $("#open-searching").click(function () {
         $("#search-input").focus()
     })
 })
+
+let $images = $(".home-news, .last-news-img");
+console.log($images);
+for (const img of $images) {
+    console.log(img.offsetWidth, img.offsetHeight);
+    let className = img.offsetWidth > img.offsetHeight ? 'longer-width' : img.offsetWidth < img.offsetHeight ? 'longer-height' : '';
+    if (className) {
+        console.log(className);
+        img.classList.add(className);
+    }
+}
